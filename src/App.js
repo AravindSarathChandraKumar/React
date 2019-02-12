44c0cc3ee3b2c092d2b9a1c9210e0f76ddb0f4df
AravindSarathChandraKumar <aravinds95@gmail.com>
2019-02-12 20:02:35 +0530

    Edit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import Todo from './components/Todo';
 class App extends Component {
+	
+	state={
+		
+		todos:[
+		{
+			id:1,
+			title:'Playing',
+			completed: false
+		},
+		{
+			id:2,
+			title:'dinner',
+			completed: false
+		},
+		{
+			id:3,
+			title:'meeting',
+			completed: false
+		},
+		
+		
+		]
+	}
   render() {
     return (
       <div className="App">
-       <Todo/>
+       <Todo todos={this.state.todos}/>
       </div>
     );
   }
